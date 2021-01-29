a94821ff376b8ce3a516118233c9a644afb94629
Juan Manuel Lopez <jlopezjuy@gmail.com>
2021-01-29 12:48:38 -0300

    Cambio en url.

--- src/main/webapp/app/app.constants.ts
+++ src/main/webapp/app/app.constants.ts
@@ -4,5 +4,5 @@
 
 export const VERSION = process.env.VERSION;
 export const DEBUG_INFO_ENABLED = false;
-export const SERVER_API_URL = process.env.SERVER_API_URL;
+export const SERVER_API_URL = 'http://mamjerez.es/';
 export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;

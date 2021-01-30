ad852170352c38e2e03836acec420919f0b00c9b
Miguel Angel Martinez <correomam@gmail.com>
2021-01-30 13:13:26 +0100

    añado comentarios en codigo.

--- src/main/webapp/app/layouts/profiles/page-ribbon.component.ts
+++ src/main/webapp/app/layouts/profiles/page-ribbon.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 import { ProfileService } from './profile.service';
 
 @Component({
+  // se usa en app>layout>main>main.component.html
   selector: 'jhi-page-ribbon',
   template: `
     <div class="ribbon" *ngIf="ribbonEnv$ | async as ribbonEnv">
@@ -18,6 +20,12 @@ export class PageRibbonComponent implements OnInit {
   constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
+    // $ es una Naming conventions for observables.
+    // https://angular.io/guide/rx-library#naming-conventions-for-observables
     this.ribbonEnv$ = this.profileService.getProfileInfo().pipe(map(profileInfo => profileInfo.ribbonEnv));
+
+    // ¿Porqué no se puede usar console.log()?
+     // eslint-disable-next-line no-console
+     console.log(this.ribbonEnv$);
   }
 }
